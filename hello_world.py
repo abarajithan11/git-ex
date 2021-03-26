f6e8346528422cca158023a1b98366209676686a
aba <abarajithan07@gmail.com>
2021-03-26 11:28:17 +0530

    Refactor - extract variable width

--- hello_world.py
+++ hello_world.py
@@ -3,11 +3,13 @@ import cv2
 print("Hello world")
 print("More code")
 
+width = 500
+
 print("First change")
 
 im = cv2.imread('det.jpg')
 
-im = cv2.resize(im, (500,500))
+im = cv2.resize(im, (width,width))
 
 cv2.imshow('file', im)
 cv2.waitKey(0)
